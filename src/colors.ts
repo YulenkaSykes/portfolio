ac13e17eefc3901a82d42196fbeb7f9b99746fa8
JuliaShandraDev <julia.shandra.dev@gmail.com>
2022-03-29 19:14:10 +0300

    fix const

--- src/colors.ts
+++ src/colors.ts
@@ -1,5 +1,5 @@
-// const blue = #0919f6;
-// const yellow = #face08;
-// const black = #000000;
-// const red = #ff0000;
-// const orange = #ff5e00;
+export const blue = "#0919f6";
+export const yellow = "#face08";
+export const black = "#000000";
+export const red = "#ff0000";
+export const orange = "#ff5e00";
